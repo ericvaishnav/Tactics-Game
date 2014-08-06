5cb89fe4b21f33548af2c703a171d047609a7c54
Mohrnee <ishan.agrawal97@gmail.com>
2014-08-05 18:04:12 -0700

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class GamePanel extends JPanel{
 	private Tile[][] tiles = new Tile[100][100];
-	private String[] types = {"Sand", "Water", "Earth"};
-	private int mag = 32;
+	private String[] types = {"Sea Water", "Sand", "Earth", "Fresh Water"};
+	private int mag = 7;
 	
 	public GamePanel(int width, int height){
 		setVisible(true);
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel{
 			for(int col = 0; col < tiles[0].length; col++){
 				Tile tile = tiles[row][col];
 				if(tile.getType().equals("Sand")) g.setColor(new Color(255,235,205));
-				if(tile.getType().equals("Water")) g.setColor(Color.BLUE);
+				if(tile.getType().contains("Water")) g.setColor(Color.BLUE);
 				if(tile.getType().equals("Earth")) g.setColor(Color.GREEN);
 				g.fillRect(col*mag, row*mag, mag, mag);
 			}
@@ -30,9 +30,31 @@ public class GamePanel extends JPanel{
 	private void setTileTypes(){
 		for(int row = 0; row < tiles.length; row++){
 			for(int col = 0; col < tiles[0].length; col++){
-				Random rand = new Random();
-				int r = rand.nextInt(3);
-				Tile tileToBeAdded = new Tile(types[r]);
+				Tile tileToBeAdded = new Tile("Earth");
+				if(row <= 3 || row >= 96 || col <= 3 || col >= 96){
+					tileToBeAdded = new Tile("Sea Water");
+				}
+				else if((row <= 22 && col <= 23) || (row <= 23 && col >= 76)){
+					tileToBeAdded = new Tile("Sea Water");
+				}
+				else if((row >= 76 && col <= 23) || (row >= 76 && col >= 76)){
+					tileToBeAdded = new Tile("Sea Water");
+				}
+				else{
+					Tile left = tiles[row][col-1];
+					Tile above = tiles[row-1][col];
+					if(left.getType().equals("Sea Water") || above.getType().equals("Sea Water")){
+						Random random = new Random();
+						int rand = random.nextInt(3);
+						if(rand < 2) tileToBeAdded = new Tile(types[0]);
+						else tileToBeAdded = new Tile(types[1]);
+					}
+					else {
+						Random random = new Random();
+						int rand = random.nextInt(2);
+						tileToBeAdded = new Tile(types[rand+1]);
+					}
+				}
 				tiles[row][col] = tileToBeAdded;
 			}
 		}
